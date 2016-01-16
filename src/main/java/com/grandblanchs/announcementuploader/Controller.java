c13fcdae779111f81cd962e992252edecb8793f9
Corey Rowe <CoreyR1196@gmail.com>
2016-01-15 23:21:30 -0500

    Added ability to remove announcements from saved list Form buttons now enable and disable when appropriate Improved text on labels

--- src/main/java/com/grandblanchs/announcementuploader/Controller.java
+++ src/main/java/com/grandblanchs/announcementuploader/Controller.java
@@ -23,21 +23,18 @@ public class Controller {
 
     public Button btn_add;
     public Button btn_edit;
+    public Button btn_remove;
     public Button btn_generate;
     public ListView<String> lst_edit;
     public TextArea txt_announcement;
     public Label lbl_number;
     public CheckBox chk_append;
 
-    public int announceNumber = 0;
-
-    public ArrayList<String> announceList = new ArrayList<>();
     public ObservableList<String> years = FXCollections.observableArrayList();
     public ObservableList<String> data = FXCollections.observableArrayList();
 
     private static final String userHomeFolder = System.getProperty("user.home");
     private static final File file = new File(userHomeFolder + "/Desktop", "Announcements.xml");
-
 
     @FXML
     void initialize() {
@@ -61,27 +58,59 @@ public class Controller {
 
         //Current year is always the sixth entry.
         cmb_year.getSelectionModel().select(5);
+
+        txt_announcement.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (txt_announcement.getText().equals("")) {
+                btn_add.setDisable(true);
+            }else{
+                btn_add.setDisable(false);
+            }
+        });
+
+        lst_edit.setOnMouseClicked(click -> {
+            if (click.getClickCount() == 2 && data.size() > 0) {
+                editAnnouncement();
+            }
+        });
     }
     public void addAnnouncement(){
 
-        announceList.add(announceNumber, txt_announcement.getText());
-        data.addAll(announceList.get(announceNumber));
+        data.add(txt_announcement.getText());
         lst_edit.setItems(data);
-        announceNumber++;
 
-        if (announceNumber == 1) {
-            lbl_number.setText(announceNumber + " Announcement");
-        }else{
-            lbl_number.setText(announceNumber + " Announcements");
-        }
+        lst_edit.getSelectionModel().selectLast();
+
+        checkNumber();
 
         txt_announcement.setText("");
 
     }
 
-    public void editAnnouncement(){
-        System.out.println(lst_edit.getSelectionModel().getSelectedItem());
+    public void checkNumber() {
+        if (data.size() > 0) {
+            btn_edit.setDisable(false);
+            btn_remove.setDisable(false);
+            btn_generate.setDisable(false);
+        }else{
+            btn_edit.setDisable(true);
+            btn_remove.setDisable(true);
+            btn_generate.setDisable(true);
+        }
 
+        if (data.size() == 1) {
+            lbl_number.setText(data.size() + " Announcement");
+        }else{
+            lbl_number.setText(data.size() + " Announcements");
+        }
+    }
+
+    public void editAnnouncement(){
+        System.out.println(lst_edit.getSelectionModel().getSelectedIndex());
+    }
+
+    public void removeAnnouncement() {
+        data.remove(lst_edit.getSelectionModel().getSelectedIndex());
+        checkNumber();
     }
 
     public void generateFile(){
@@ -89,10 +118,4 @@ public class Controller {
 
         }
     }
-
-
-    public void doneEditing(){
-
-    }
-
 }
