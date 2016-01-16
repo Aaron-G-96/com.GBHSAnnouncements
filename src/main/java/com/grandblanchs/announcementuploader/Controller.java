14cb4cd374a3f25042275f5184206fa162537456
Corey Rowe <CoreyR1196@gmail.com>
2016-01-15 21:15:34 -0500

    Made date selection automatic and dynamic

--- src/main/java/com/grandblanchs/announcementuploader/Controller.java
+++ src/main/java/com/grandblanchs/announcementuploader/Controller.java
@@ -2,6 +2,7 @@ package com.grandblanchs.announcementuploader;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -12,7 +13,7 @@ import javafx.stage.Stage;
 
 import javax.swing.*;
 import java.io.File;
-import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class Controller {
@@ -30,11 +31,36 @@ public class Controller {
     public int announceNumber = 0;
 
     public ArrayList<String> announceList = new ArrayList<>();
+    public ObservableList<String> years = FXCollections.observableArrayList();
     public ObservableList<String> data = FXCollections.observableArrayList();
 
     private static final String userHomeFolder = System.getProperty("user.home");
     private static final File file = new File(userHomeFolder + "/Desktop", "Announcements.xml");
 
+
+    @FXML
+    void initialize() {
+        LocalDateTime ldt = LocalDateTime.now();
+
+        //Add the previous and next five years to the "Years" combo box.
+        ldt = ldt.minusYears(5);
+        for (int i = 0; i < 11; i++) {
+            years.add(String.valueOf(ldt.getYear()));
+            ldt = ldt.plusYears(1);
+        }
+
+        cmb_year.setItems(years);
+
+        //Reset to now
+        ldt = LocalDateTime.now();
+
+        //Select the current date.
+        cmb_day.getSelectionModel().select(ldt.getDayOfMonth() - 1);
+        cmb_month.getSelectionModel().select(ldt.getMonthValue() - 1);
+
+        //Current year is always the sixth entry.
+        cmb_year.getSelectionModel().select(5);
+    }
     public void addAnnouncement(){
 
         announceList.add(announceNumber, txt_announcement.getText());
