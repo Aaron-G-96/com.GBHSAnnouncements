7b2eabe783839b4c01eec6269d9a604ff181575c
Corey Rowe <CoreyR1196@gmail.com>
2016-01-15 21:35:37 -0500

    Added icon

--- src/main/java/com/grandblanchs/announcementuploader/Main.java
+++ src/main/java/com/grandblanchs/announcementuploader/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -18,6 +19,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/view/main.fxml"));
         primaryStage.setTitle("GBHS Announcement Uploader (Beta)");
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/image/icon.png")));
         primaryStage.setScene(new Scene(root, 1280, 720));
         primaryStage.show();
     }
